646a3d034e556ade03c7421d04954ba304a2f037
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2023-12-14 21:39:06 +0800

    update

---
+++ resources/views/panel/base.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel Control</title>
+
+    <link rel="stylesheet" href="{{ base_path('node_modules/bootstrap/dist/css/bootstrap.min.css') }}">
+</head>
+
+<body>
+    <table class="table table-bordered">
+        <thead>
+            <th>No</th>
+            <th>Nama</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>IBNU</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <script src="{{ base_path('node_modules/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+</body>
+
+</html>
